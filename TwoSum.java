87ce5c29c1de6f1e9a1f08b636ef44ab508bc234
Niteeshh16 <niteeshhegde343@gmail.com>
2025-07-15 22:30:39 +0530

    Updated

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class TwoSum {
+    static int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int pair = target - nums[i];
+            if (map.containsKey(pair)){
+                return new int[]{map.get(pair),i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+            int nums[] = {1,6,2,10,3};
+            int target = 7;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+}
